c12942ab84324c83a3522189fdd0a9d275c20f37
hannes.sachsenhofer <hannes.sachsenhofer@1ebb5cdc-31bd-11de-9b58-cfb382c7a5ee>
2009-06-05 11:23:12 +0000

    Added a "reply" option to the Twitter plugin.

---
+++ Saxx.Carmine.Plugins.Twitter/Tweet.cs
@@ -0,0 +1,26 @@
+﻿
+namespace Saxx.Carmine.Plugins {
+    internal class Tweet {
+
+        public Tweet(string from, string status, int id) {
+            From = from;
+            Status = status;
+            Id = id;
+        }
+
+        public string From {
+            get;
+            private set;
+        }
+
+        public string Status {
+            get;
+            private set;
+        }
+
+        public int Id {
+            get;
+            private set;
+        }
+    }
+}
--- Saxx.Carmine.Plugins.Twitter/Twitter.cs
+++ Saxx.Carmine.Plugins.Twitter/Twitter.cs
@@ -5,6 +5,8 @@ using System.Net;
 using System.Text.RegularExpressions;
 using System.Web;
 using System.Xml.Linq;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace Saxx.Carmine.Plugins {
     public partial class Twitter : Plugin {
@@ -31,6 +33,24 @@ namespace Saxx.Carmine.Plugins {
                 return;
             }
 
+            match = Regex.Match(message, "^twitter reply(?<countBack> \\d{1,2})? (?<status>.*?)$", RegexOptions.IgnoreCase);
+            if (match.Success) {
+                var status = match.Groups["status"].Value.Trim();
+                var countBack = string.IsNullOrEmpty(match.Groups["countBack"].Value.Trim()) ? 1 : Convert.ToInt32(match.Groups["countBack"].Value.Trim());
+                if (!string.IsNullOrEmpty(status)) {
+                    var userData = GetUserData();
+                    var user = userData.FirstOrDefault(x => x.Jid.Equals(from, StringComparison.InvariantCultureIgnoreCase));
+                    if (user != null) {
+                        Reply(user, countBack, status);
+                        WriteTimeline(user, false);
+                        SetUserData(userData);
+                    }
+                    else
+                        SendMessage(from, "You have to register your twitter credentials first.");
+                }
+                return;
+            }
+
             match = Regex.Match(message, "^twitter (?<status>.*?)$", RegexOptions.IgnoreCase);
             if (match.Success) {
                 var status = match.Groups["status"].Value.Trim();
@@ -38,7 +58,7 @@ namespace Saxx.Carmine.Plugins {
                     var userData = GetUserData();
                     var user = userData.FirstOrDefault(x => x.Jid.Equals(from, StringComparison.InvariantCultureIgnoreCase));
                     if (user != null) {
-                        SetStatus(user, status);
+                        SetStatus(user, status, 0);
                         WriteTimeline(user, false);
                         SetUserData(userData);
                     }
@@ -79,15 +99,19 @@ namespace Saxx.Carmine.Plugins {
             }
         }
 
-        private void SetStatus(UserData user, string status) {
+        private void SetStatus(UserData user, string status, int replyTo) {
             try {
                 var request = WebRequest.Create("http://twitter.com/statuses/update.xml");
                 request.Credentials = new NetworkCredential(user.UserName, user.Password);
                 request.Method = "POST";
                 request.ContentType = "application/x-www-form-urlencoded";
 
-                using (var writer = new StreamWriter(request.GetRequestStream()))
+                using (var writer = new StreamWriter(request.GetRequestStream())) {
                     writer.Write("status=" + HttpUtility.UrlEncode(status));
+
+                    if (replyTo > 0)
+                        writer.Write("&in_reply_to_status_id=" + HttpUtility.UrlEncode(replyTo.ToString()));
+                }
 
                 using (var reader = new StreamReader(request.GetResponse().GetResponseStream()))
                     reader.ReadToEnd();
@@ -98,12 +122,39 @@ namespace Saxx.Carmine.Plugins {
             }
         }
 
+        private void Reply(UserData user, int countBack, string status) {
+            var tweets = GetTweets(user, 0).OrderByDescending(x => x.Id);
+            countBack--;
+            if (countBack >= tweets.Count())
+                countBack = 0;
+            var tweetToReply = tweets.Skip(countBack).First();
+            SetStatus(user, "@" + tweetToReply.From + " " + status, tweetToReply.Id);
+        }
+
         private void WriteTimeline(UserData user, bool answerAlways) {
+            var tweets = GetTweets(user, user.LastId);
+            var message = "";
+
+            foreach (var tweet in tweets) {
+                message += "*@" + tweet.From + ":* " + tweet.Status + Environment.NewLine + Environment.NewLine;
+                user.LastId = Math.Max(user.LastId, tweet.Id);
+            }
+
+            message = message.Trim();
+
+            if (!string.IsNullOrEmpty(message))
+                SendMessage(user.Jid, message);
+            else if (answerAlways)
+                SendMessage(user.Jid, "No new statuses available.");
+        }
+
+        private IEnumerable<Tweet> GetTweets(UserData user, int startAt) {
+            var tweets = new List<Tweet>();
+
             try {
-                var request = WebRequest.Create("http://twitter.com/statuses/friends_timeline.xml" + (user.LastId > 0 ? "?since_id=" + user.LastId : ""));
+                var request = WebRequest.Create("http://twitter.com/statuses/friends_timeline.xml" + (startAt > 0 ? "?since_id=" + startAt : ""));
                 request.Credentials = new NetworkCredential(user.UserName, user.Password);
 
-                var message = "";
                 using (var reader = new StreamReader(request.GetResponse().GetResponseStream())) {
                     var xml = XDocument.Load(reader);
                     foreach (var element in xml.Root.Elements("status")) {
@@ -111,23 +162,18 @@ namespace Saxx.Carmine.Plugins {
                         var from = element.Element("user").Element("screen_name").Value;
                         var id = Convert.ToInt32(element.Element("id").Value);
 
-                        message = "*@" + from + ":* " + text + Environment.NewLine + Environment.NewLine + message;
-
-                        user.LastId = Math.Max(user.LastId, id);
+                        tweets.Add(new Tweet(from, text, id));
                     }
                 }
 
-                message = message.Trim();
-
-                if (!string.IsNullOrEmpty(message))
-                    SendMessage(user.Jid, message);
-                else if (answerAlways)
-                    SendMessage(user.Jid, "No new statuses available.");
             }
             catch (Exception ex) {
                 SendMessage(user.Jid, "Error while downloading your Twitter timeline: " + ex.Message);
                 Log(LogType.Error, "Error while downloading Twitter timeline.", ex);
             }
+
+            tweets = tweets.OrderBy(x => x.Id).ToList();
+            return tweets;
         }
 
         public override string ToString() {
